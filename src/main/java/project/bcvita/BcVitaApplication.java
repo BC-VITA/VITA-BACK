32e362fb8029c132d72e474d240f489b73c684a2
lby9906 <lby9906@naver.com>
2023-01-15 11:56:08 +0900

    환경설정 최종

---
+++ src/main/java/project/bcvita/BcVitaApplication.java
@@ -0,0 +1,13 @@
+package project.bcvita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BcVitaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BcVitaApplication.class, args);
+    }
+
+}
