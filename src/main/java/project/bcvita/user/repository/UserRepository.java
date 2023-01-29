6e11cbe40f468d03208bbd33789140d8d378e2a2
AppleMeldoy <applejam523@gmail.com>
2023-01-29 16:33:26 +0900

    로그인 구현

--- src/main/java/project/bcvita/user/repository/UserRepository.java
+++ src/main/java/project/bcvita/user/repository/UserRepository.java
@@ -1,17 +1,14 @@
 package project.bcvita.user.repository;
 
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import project.bcvita.user.entity.User;
 
-import java.lang.reflect.Member;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
-    Optional<User> findByUserID(String userID);
+    User findByUserID(String userID);
     Optional<User> findByUserPW(String userPW);
 
 }
