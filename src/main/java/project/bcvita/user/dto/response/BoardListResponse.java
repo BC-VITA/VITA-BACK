37dd1d4f214b3ac33df2e96243c029e4afe8accd
lby9906 <lby9906@naver.com>
2023-02-19 23:43:41 +0900

    지정헌혈 게시판 수정

--- src/main/java/project/bcvita/user/dto/response/BoardListResponse.java
+++ src/main/java/project/bcvita/user/dto/response/BoardListResponse.java
@@ -12,11 +12,9 @@ import javax.persistence.OneToOne;
 @Getter
 public class BoardListResponse {
 
-    private String hospitalName;  //DesignatedBloodWrite 이런식으로 그 내가 api를 두개로 해서 나눠놓은거야 이게 저 엔티티고 아까 내가 하나 있던건 저기서 user붙인 엔티티 이해했어? 그럼
-    //DesignatedBloodWriteUser 에 필요한거 여기다가 다 적고
-    //DesignatedBloodWrite 이것도 여기다가 다 적어봐
+    private String hospitalName;
 
-    private String title; //DesignatedBloodWrite 이런식으로 붙여줘 어떤거 어디 엔티티인
+    private String title;
 
     private String content;
 
@@ -28,6 +26,6 @@ public class BoardListResponse {
 
     private Long DesignatedBloodWriteNumber;
 
-    private Integer bloodNumber; //DesignatedBloodWriteUser 이것만 이 엔티티고 나머지는 user안붙인 엔티티얌 오켕
+    private Integer bloodNumber;
 
 }
