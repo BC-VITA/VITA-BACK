e8cd541b7d16d7031d8e67b00f22735fc97be60e
lby9906 <lby9906@naver.com>
2023-06-18 01:48:08 +0900

    봉사 신청 api

---
+++ src/main/java/project/bcvita/user/dto/response/VolunteerReservationSaveResponseDto.java
@@ -0,0 +1,25 @@
+package project.bcvita.user.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDate;
+
+@Getter
+@AllArgsConstructor
+public class VolunteerReservationSaveResponseDto {
+    private String status;
+    private String volunteerDate;
+
+    private String startTime;
+
+    private String endTime;
+
+    private String address;
+    private String volunteerPlace;
+    private String volunteerType;
+    private String name;
+    private String phone;
+}
+
