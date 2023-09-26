e96181fde952b3ec9ccbce2aff2a3f483ffdacf1
lby9906 <lby9906@naver.com>
2023-09-26 11:32:00 +0900

    따뜻한 게시물 list api

---
+++ src/main/java/project/bcvita/user/dto/response/AdminWarmCaseResponse.java
@@ -0,0 +1,15 @@
+package project.bcvita.user.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class AdminWarmCaseResponse {
+
+    private String title;
+
+    private String content;
+
+    private String imageUrl;
+}
