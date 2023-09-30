c91a69b1aafdc8e0981d24e6ed56479198b998bc
lby9906 <lby9906@naver.com>
2023-09-30 18:12:58 +0900

    봉사 통계 api 구현

--- src/main/java/project/bcvita/user/dto/response/AdminVolunteerStatisticsInterface.java
+++ src/main/java/project/bcvita/user/dto/response/AdminVolunteerStatisticsInterface.java
@@ -1,9 +1,12 @@
 package project.bcvita.user.dto.response;
 
+import lombok.Getter;
+
+@Getter
 public class AdminVolunteerStatisticsInterface {
 
-    private Integer year;
-    private Integer month;
+    private Long year;
+    private Long month;
     private Long count;
 
 }
--- src/main/java/project/bcvita/user/repository/VolunteerReservationRepository.java
+++ src/main/java/project/bcvita/user/repository/VolunteerReservationRepository.java
@@ -2,11 +2,13 @@ package project.bcvita.user.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import project.bcvita.user.dto.response.AdminVolunteerStatisticsInterface;
 import project.bcvita.user.entity.User;
 import project.bcvita.user.entity.VolunteerRegister;
 import project.bcvita.user.entity.VolunteerReservation;
+import project.bcvita.user.service.DateStatistics;
 
 import java.util.List;
 
@@ -21,15 +23,15 @@ public interface VolunteerReservationRepository extends JpaRepository<VolunteerR
 
     List<VolunteerReservation> findById(String boardId);
 
-//    @Query("SELECT FUNCTION('EXTRACT', YEAR, vr.volunteerDate) AS year, " +
-//            "FUNCTION('EXTRACT', MONTH, vr.volunteerDate) AS month, " +
-//            "COUNT(*) AS count " +
-//            "FROM VolunteerReservation vr " +
-//            "WHERE vr.boardStatus = '참여완료' " +
-//            "GROUP BY FUNCTION('EXTRACT', YEAR, vr.volunteerDate), FUNCTION('EXTRACT', MONTH, vr.volunteerDate) " +
-//            "ORDER BY year, month")
-//    List<AdminVolunteerStatisticsInterface> getMonthlyVolunteerStatistics();
-//
-//    List<VolunteerReservation> findByBoardStatusOrderByVolunteerDateAsc(String boardStatus);
+    @Query(value = "SELECT EXTRACT(YEAR FROM volunteer_date) AS year, " +
+            "EXTRACT(MONTH FROM volunteer_date) AS month, " +
+    " COUNT(*) AS count " +
+    "FROM volunteer_reservation " +
+    "WHERE board_status = '참여완료' AND EXTRACT(YEAR FROM volunteer_date) = :year " +
+    "GROUP BY EXTRACT(YEAR FROM volunteer_date), EXTRACT(MONTH FROM volunteer_date) " +
+    "ORDER BY EXTRACT(YEAR FROM volunteer_date), EXTRACT(MONTH FROM volunteer_date) ", nativeQuery = true)
+    List<DateStatistics> getMonthlyVolunteerStatistics(@Param("year") int year);
+
+    List<VolunteerReservation> findByBoardStatusOrderByVolunteerDateAsc(String boardStatus);
 
 }
