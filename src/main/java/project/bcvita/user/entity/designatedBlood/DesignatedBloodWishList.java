e9988d6a004d5f8e89e3ef44b1d7f356252464a7
lby9906 <lby9906@naver.com>
2024-01-14 21:02:23 +0900

    fix : 지정헌혈에 관한 패키지 분리

--- src/main/java/project/bcvita/user/entity/designatedBlood/DesignatedBloodWishList.java
+++ src/main/java/project/bcvita/user/entity/designatedBlood/DesignatedBloodWishList.java
@@ -1,9 +1,10 @@
-package project.bcvita.user.entity;
+package project.bcvita.user.entity.designatedBlood;
 
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import project.bcvita.user.entity.User;
 
 import javax.persistence.*;
 
